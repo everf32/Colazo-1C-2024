ea05aae41ba3121c8c652f0e92da428c03bf73bd
everf32 <everf97@live.com>
2024-05-15 10:11:50 -0300

    Se agrega el esqueleto de proyecto integrador

---
+++ firmware/projects/Proyecto_Integrador_Colazo/main/Proyecto_Integrador_Colazo.c
@@ -0,0 +1,42 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 20/03/2024 | Document creation		                         |
+ *
+ * @author Ever Colazo (everf97@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <led.h>
+#include <gpio_mcu.h>
+#include <ble>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
