22a2eaecdc0904a9c4367a9422fdc1d714d1ff0f
everf32 <everf97@live.com>
2024-05-22 11:20:20 -0300

    Se actualiza el punto c del proyecto integrador con avances

--- firmware/projects/Proyecto_Integrador_Colazo/main/Proyecto_Integrador_Colazo.c
+++ firmware/projects/Proyecto_Integrador_Colazo/main/Proyecto_Integrador_Colazo.c
@@ -21,7 +21,6 @@
  * @author Ever Colazo (everf97@gmail.com)
  *
  */
-
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
@@ -37,121 +36,180 @@
 #include <analog_io_mcu.h>
 
 /*==================[macros and definitions]=================================*/
-#define PERIOD 100
+#define PERIODA 100
+#define PERIODB 200
+#define PERIODC 300
+#define PERIODD 400
+#define UPPER_THRESHOLD 8.0 // Umbral superior para temperatura
+#define LOWER_THRESHOLD 2.0 // Umbral inferior para temperatura
+
+
 
 /*==================[internal data definition]===============================*/
 TaskHandle_t conversorAD_task_handle = NULL;
 TaskHandle_t comparar_task_handle = NULL;
 TaskHandle_t mostrar_task_handle = NULL;
 TaskHandle_t procesar_task_handle = NULL;
-
+uint8_t temperature = 0;
+uint8_t temperatures[10] = {5 , 6 , 4 , 7 , 2 , 5 , 3 , 4 , 5 , 2};
 bool On;
 
 /*==================[internal functions declaration]=========================*/
 
 void leerDato(uint8_t *dato, uint8_t tamanio)
 {
-	// funcion encargada de registrar los datos que se reciben por bluetooth
+	// Función a ejecutarse ante un interrupción de recepción a través de la conexión BLE.
 }
 
 void FuncTimerProcesar()
 {
-	vTaskNotifyGiveFromISR(procesar_task_handle,pdFALSE);
+	vTaskNotifyGiveFromISR(procesar_task_handle, pdFALSE);
 }
 
 void FuncTimerMostrar()
 {
-vTaskNotifyGiveFromISR(mostrar_task_handle,pdFALSE);
+	vTaskNotifyGiveFromISR(mostrar_task_handle, pdFALSE);
 }
 
 void FuncTimerConversorAD()
 {
-	vTaskNotifyGiveFromISR(comparar_task_handle,pdFALSE);
+	vTaskNotifyGiveFromISR(comparar_task_handle, pdFALSE);
 }
 void FuncTimerComparar()
 {
-	vTaskNotifyGiveFromISR(conversorAD_task_handle,pdFALSE);
+	vTaskNotifyGiveFromISR(conversorAD_task_handle, pdFALSE);
 }
 
 void ProcesarTask()
 {
-	while(true)
+	while (true)
 	{
-	// tarea encargada de aplicar diferentes procesamientos a las señales de entrada: temperatura,humedad, luz
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		// tarea encargada de aplicar diferentes procesamientos a las señales de entrada: temperatura,humedad, luz
 	}
 }
 
 void compararTask()
 {
-	while(true)
+	while (true)
 	{
-	/* tarea encargada de comparar valores actuales de temperatura, humedad y luz versus diferentes valores umbrales
-	ya seteados
-	*/
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		/* tarea encargada de comparar valores actuales de temperatura, humedad y luz versus diferentes valores umbrales
+		ya seteados
+		*/
+
+		/*
+			if (temperature > UPPER_THRESHOLD || temperature < LOWER_THRESHOLD)
+			{
+				// Encender el buzzer
+				gpio_set_level(PIN_BUZZER, 1);
+			}
+			else
+			{
+				// Apagar el buzzer
+				gpio_set_level(PIN_BUZZER, 0);
+			}
+		*/
 	}
 }
 
 void mostrarTask()
 {
-	while(true)
+	while (true)
 	{
-// tarea encargada de encender la tira led para el valor de temperatura
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		// tarea encargada de encender la tira led para el valor de temperatura
 	}
-	
 }
 
 void conversorADTask()
 {
+	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	// tarea dedicada a convertir los datos de temperatura y luz de analogico a digital
+
+	/*
+		temperature = analogRead(PIN_MCP9700) * (3.3 / 4095.0) * 100;
+		humidity = dht_read_humidity();
+		lightLevel = analogRead(PIN_LDR);
+	*/
 }
+
+void medirTask()
+{
+	
+}
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
 
-	ble_config_t ble_configuration = {
-		.device_name = "ESP_EDU_1",
+	ble_config_t ble_config = {
+		.device_name = "Seguridad de vacunas",
 		.func_p = leerDato};
-
-	
-		timer_config_t ProcesarTask = {
+	/*
+		timer_config_t timerProcesar = {
 			.timer = TIMER_A,
-			.period = PERIOD,
+			.period = PERIODA,
 			.func_p = FuncTimerProcesar,
 			.param_p = NULL};
 
-		timer_config_t compararTask = {
+		timer_config_t timerComparar = {
 			.timer = TIMER_B,
-			.period = PERIOD,
+			.period = PERIODB,
 			.func_p = FuncTimerComparar,
 			.param_p = NULL};
 
-		timer_config_t conversorADTask = {
+		timer_config_t timerConversorAD = {
 			.timer = TIMER_C,
-			.period = PERIOD,
+			.period = PERIODC,
 			.func_p = FuncTimerConversorAD,
 			.param_p = NULL};
 
-			timer_config_t mostrarTaskTask = {
+		timer_config_t TimerMostrar = {
 			.timer = TIMER_D,
-			.period = PERIOD,
+			.period = PERIODD,
 			.func_p = FuncTimerMostrar,
 			.param_p = NULL};
-	
-	
+	*/
+	/*--------------------Inicializacion de dispositivos---------------------*/
 
-	/*--------------------Inicializacion--------------------*/
 	/*
 
 		NeoPixelInit(BUILT_IN_RGB_LED_PIN, BUILT_IN_RGB_LED_LENGTH, &color);
 		NeoPixelAllOff();
 
 	*/
-	BleInit(&ble_configuration);
+	LedsInit();
+	BleInit(&ble_config);
 
+	/*Para probar la conexion bluetooth
+	while (1)
+	{
+		vTaskDelay(PERIODD / portTICK_PERIOD_MS);
+		switch (BleStatus())
+		{
+		case BLE_OFF:
+			LedOn(LED_2);
+			break;
+		case BLE_DISCONNECTED:
+			LedsOffAll();
+			break;
+		case BLE_CONNECTED:
+			LedToggle(LED_1);
+			break;
+		}
+	}
+     */
 	/*creacion de tareas*/
 	xTaskCreate(&ProcesarTask, "procesar", 512, NULL, 5, &procesar_task_handle);
 	xTaskCreate(&mostrarTask, "mostrar", 512, NULL, 5, &mostrar_task_handle);
 	xTaskCreate(&conversorADTask, "conversor", 512, NULL, 5, &conversorAD_task_handle);
 	xTaskCreate(&compararTask, "comparar", 512, NULL, 5, &comparar_task_handle);
+
+	/*Inicialización de timers*/
+	// TimerStart(timerComparar.timer);
+	// TimerStart(TimerMostrar.timer);
+	// TimerStart(timerConversorAD.timer);
+	// TimerStart(timerProcesar.timer);
 }
 /*==================[end of file]============================================*/
